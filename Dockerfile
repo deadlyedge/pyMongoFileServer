f4ebd29a35e7c3e91c03f8e903748e76c5305e41
xdream oldlu <xdream@gmail.com>
2024-01-04 01:40:35 +0800

    nice!

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.12-alpine
+LABEL maintainer="xdream oldlu <xdream@gmail.com>"
+
+WORKDIR /app
+
+COPY ./requirements.txt /app/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
+
+# 
+COPY ./app /app/
+
+# 
+CMD ["uvicorn", "main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "8001"]
--- app/main.py
+++ app/main.py
@@ -1,40 +1,101 @@
 import os
-import uuid
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import HTMLResponse, StreamingResponse
+import pendulum
+from fastapi import Depends, FastAPI, HTTPException, UploadFile, File, status
+from fastapi.responses import HTMLResponse, StreamingResponse, JSONResponse
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
 from pymongo import MongoClient
 from gridfs import GridFS
 from bson import ObjectId
+from loguru import logger
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
-
-app = FastAPI()
+API_KEYS = os.getenv("XD_API_KEY")
+if API_KEYS is None:
+    raise ValueError("XD_API_KEY environment variable not set")
 
 # Connect to MongoDB
 client = MongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017"))
 db = client[os.getenv("MONGO_DB_NAME", "myFiles")]
 fs = GridFS(db)
 
+# use token authentication
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.post("/upload/")
+app = FastAPI()
+
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key not in API_KEYS:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden"
+        )
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/upload/", dependencies=[Depends(api_key_auth)])
 async def upload_image(file: UploadFile = File(...)):
     # Read the file content
     contents = await file.read()
 
     # Store the file in GridFS
     file_id = fs.put(contents, filename=file.filename)
+    logger.info(
+        f"{file.filename}, {round((file.size or 1) / 1024 / 1024, 2)} MB, uploaded."
+    )
 
-    return {
-        "file_id": str(file_id),
-        "file_url": f"{BASE_URL}/file/{file_id}/",
-        "show_image": f"{BASE_URL}/file/{file_id}/?output=html",
-    }
+    return JSONResponse(
+        {
+            "file_id": str(file_id),
+            "file_url": f"{BASE_URL}/get/{file_id}/",
+            "show_image": f"{BASE_URL}/get/{file_id}/?output=html",
+        },
+        status_code=200,
+    )
 
 
-@app.get("/file/{file_id}/")
+class ToDeleteProps(BaseModel):
+    file_ids: list[str]
+
+
+@app.post("/delete/", dependencies=[Depends(api_key_auth)])
+async def delete_image(to_be_delete: ToDeleteProps):
+    # Delete the files from GridFS
+    for file_id in to_be_delete.file_ids:
+        fs.delete(ObjectId(file_id))
+        logger.info(f"File {file_id} deleted.")
+
+    return JSONResponse({"message": "Files deleted successfully."}, status_code=200)
+
+
+@app.get("/list/", dependencies=[Depends(api_key_auth)])
+async def list_files():
+    # Retrieve all files from GridFS
+    files = fs.find()
+
+    files_list = [
+        {
+            "filename": file.filename,
+            "id": str(file._id),
+            "size": file.length,
+            "delta_time": pendulum.instance(file.upload_date).diff().seconds,
+        }
+        for file in files
+    ]
+
+    logger.info(f"{len(files_list)} files in DATABASE")
+
+    return JSONResponse(files_list, status_code=200)
+
+
+@app.get("/get/{file_id}/")
 async def get(file_id: str, output: str | None = "src"):
     # Retrieve the file from GridFS
     file = fs.get(ObjectId(file_id))
@@ -59,7 +120,7 @@ async def get(file_id: str, output: str | None = "src"):
                 </head>
                 <body>
                     <h1>Image{file.name}</h1>
-                    <img src="/file/{file_id}/" alt="Uploaded Image" style="max-width:100%">
+                    <img src="/get/{file_id}/" alt="Uploaded Image" style="max-width:100%">
                 </body>
                 </html>
                 """
@@ -72,57 +133,6 @@ async def get(file_id: str, output: str | None = "src"):
                 )
     else:
         return {"error": "File not found"}
-
-
-# @app.get("/download/{file_id}")
-# async def get_file(file_id: str):
-#     # Retrieve the file from GridFS
-#     file = fs.get(ObjectId(file_id))
-#     if file:
-#         # Return the file contents as a streaming response
-#         return StreamingResponse(
-#             iter([file.read()]),
-#             media_type="application/octet-stream",
-#             headers={"Content-Disposition": f"attachment; filename={file.filename}"},
-#         )
-#     else:
-#         return {"error": "File not found"}
-
-
-# @app.get("/image/{file_id}")
-# async def get_image_data(file_id: str):
-#     # Retrieve the file from GridFS
-#     file = fs.get(ObjectId(file_id))
-#     if file:
-#         # Return the file contents as a streaming response with the appropriate content type
-#         return StreamingResponse(
-#             iter([file.read()]), media_type="application/octet-stream"
-#         )
-#     else:
-#         return {"error": "File not found"}
-
-
-# @app.get("/show_image/{file_id}")
-# async def show_image(file_id: str):
-#     # Retrieve the file from GridFS
-#     file = fs.get(ObjectId(file_id))
-#     if file:
-#         # Return an HTML page with the image embedded
-#         html_content = f"""
-#         <!DOCTYPE html>
-#         <html>
-#         <head>
-#             <title>Image</title>
-#         </head>
-#         <body>
-#             <h1>Image{file.name}</h1>
-#             <img src="/image/{file_id}" alt="Uploaded Image" style="max-width:100%">
-#         </body>
-#         </html>
-#         """
-#         return HTMLResponse(content=html_content, status_code=200)
-#     else:
-#         return HTMLResponse(content="File not found", status_code=404)
 
 
 if __name__ == "__main__":
